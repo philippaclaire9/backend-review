930d59aae69d523a5c03e02a023f879274a58703
Philippa <philippa.claire9@gmail.com>
2020-03-23 09:40:22 +0000

    enable cors

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-const express = require("express");
+const express = require('express');
+const cors = require('cors');
 
-const apiRouter = require("./routes/api");
+const apiRouter = require('./routes/api');
 const {
   handles404s,
   handles400s,
@@ -8,14 +9,17 @@ const {
   handlesStatusErrors,
   handles500s,
   handles405s
-} = require("./errors");
+} = require('./errors');
+
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 
-app.use("/api", apiRouter);
+app.use('/api', apiRouter);
 
-app.all("/*", handles404s);
+app.all('/*', handles404s);
 
 app.use(handles400s);
 
@@ -24,5 +28,7 @@ app.use(handles422s);
 app.use(handlesStatusErrors);
 
 app.use(handles500s);
+
+app.use(handles405s);
 
 module.exports = app;
