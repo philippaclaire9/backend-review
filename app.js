8f9b314e6113f9f12f16091f74f0c6efdda3611e
Philippa <philippa.claire9@gmail.com>
2020-03-23 09:46:06 +0000

    attempt to fix cors error

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+
 const cors = require('cors');
 
 const apiRouter = require('./routes/api');
@@ -13,9 +14,9 @@ const {
 
 const app = express();
 
-app.use(cors());
-
 app.use(express.json());
+
+app.use(cors());
 
 app.use('/api', apiRouter);
 
